7d86151e45a5922b50a62c49a0ee19e4932b6bc3
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-11 23:43:30 +0100

    Triangle area

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-﻿var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
-
-
-var a = 5,
+﻿var a = 5,
     h = 6,
     triangleArea = a*h/2;
 console.log('Pole trójkąta o podstawie a: ' + a +' i wysokości h: ' + h +' wynosi: ' + triangleArea);
