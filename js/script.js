4322ff8f706a6d7d1024c0b052e1221b70bf27d3
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-11 23:10:05 +0100

    Conditional

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 ﻿var a = -4,
-	b =  5,
-	value = (a*a) + (2 * a * b) - (b*b);
-console.log(value)
+		b =  5,
+		value = (a*a) + (2 * a * b) - (b*b);
+console.log(value);
 
 if ( value > 0 ) {
 	console.log('Wynik dodatni');
@@ -10,4 +10,4 @@ else {
 	console.log('Wynik ujemny');
 }
 
-var value = 0 ? console.log('Wynik równy zero') : console.log('Wynik różny od zera')
+value == 0 ? console.log('Wynik równy zero') : console.log('Wynik różny od zera')
