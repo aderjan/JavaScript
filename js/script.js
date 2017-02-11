8c0a61aed2b05c875e083b2959a11832b517dde6
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-11 22:26:30 +0100

    Conditional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-﻿var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+﻿var a = -4,
+	b =  5,
+	value = (a*a) + (2 * a * b) - (b*b);
+console.log(value)
+
+if ( value > 0 ) {
+	console.log('Wynik dodatni');
+}
+else {
+	console.log('Wynik ujemny');
+}
+
+var value = 0 ? console.log('Wynik równy zero') : console.log('Wynik różny od zera')
