7d35c9aab77d30d89a1a425241f5265a6c89b6af
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-12 11:47:20 +0100

    Triangle area function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-﻿var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+﻿function getTriangleArea(a, h) {
+	
+	var value = (a*h/2);
+	
+	if ( (a > 0) && (h > 0) ) {
+		return(value);
+	}
+
+	else {
+		return('Nieprawidłowe dane');
+	}
+	
+	var triangle1Area = getTriangleArea(10, 15),
+		triangle2Area = getTriangleArea(-10, 15),
+		triangle3Area = getTriangleArea(5, 8);
+
+}
+
+console.log( getTriangleArea(10,6) );
+console.log( getTriangleArea(10,15) );
+console.log( getTriangleArea(-10,15) );
+console.log( getTriangleArea(5,8) );
+
+
