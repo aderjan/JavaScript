124c8b7be3a6dee3ce9b83e7a9d26a0d3c389c1a
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-12 13:48:13 +0100

    Strings

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-﻿var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+﻿var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.',
+    newText = 'Zielone słonie',
+    animal = newText.toUpperCase(),
+    textAfter = text.replace('Papugi', animal);
+
+//console.log(textAfter.length); 
+
+var halfOfTextAfter = textAfter.slice(0, 45);
+console.log(halfOfTextAfter);
