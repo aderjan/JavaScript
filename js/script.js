f72d33d4113c63585ee9a35a8bdf5511b547c714
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-13 20:37:47 +0100

    Buttons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 ﻿var list = document.getElementById('list'),
     add = document.getElementById('addElem'),
     elementNumber = document.getElementsByClassName('item').length+1;
-
 add.addEventListener('click', function() {
 	list.innerHTML += '<li>item ' + elementNumber + '</li>'
 	elementNumber++
