1fe67389c640a9169473de06bf2b69515df44872
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-13 10:59:31 +0100

    christmas tree

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-﻿var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+﻿function rysujChoinkę(n) {
+	
+
+    for ( var n = 0 ; n <= 5 ; n++) {
+        var star = '';
+        
+        for (var j=0 ; j <= (n*2 - 1) ; j++) {
+            var star = '';
+            star =  star + '*';
+        }
+    console.log(star)
+    }
+}
+var n = 1;
+console.log( rysujChoinkę(n) );
