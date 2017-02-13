069a427891d826ac632fcab82fa93fe79076be36
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-13 11:09:13 +0100

    Christmass Tree

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-﻿function rysujChoinkę(n) {
-	
+﻿var n = 5;
 
-    for ( var n = 0 ; n <= 5 ; n++) {
+function rysujChoinkę(n) {
+	
+    for ( var n = 1 ; n <= 5 ; n++) {
         var star = '';
         
-        for (var j=0 ; j <= (n*2 - 1) ; j++) {
-            var star = '';
-            star =  star + '*';
+        for (var j = 1 ; j <= (n*2 - 1) ; j++) {
+            star = star + '*';
         }
-    console.log(star)
+    console.log(star);
     }
 }
-var n = 1;
+
 console.log( rysujChoinkę(n) );
