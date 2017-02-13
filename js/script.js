fa58e813bb361f682521434db18f20168b679976
Adrian <adriant132@gmail.com>
2017-02-13 20:36:32 +0100

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-﻿var list = document.getElementById('list'),
-    add = document.getElementById('addElem'),
-    elementNumber = document.getElementsByClassName('item').length+1;
-
-add.addEventListener('click', function() {
-	list.innerHTML += '<li>item ' + elementNumber + '</li>'
-	elementNumber++
-});
