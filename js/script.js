a4e7aed376f1e08b0c6baa304a72d6e658313bc9
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-14 19:47:00 +0100

    Triangle Area

--- js/script.js
+++ js/script.js
@@ -10,15 +10,17 @@
 	    return 'Nieprawidłowe dane';
 	}
 
-	var triangle1Area = getTriangleArea(10, 15),
-	    triangle2Area = getTriangleArea(-10, 15),
-	    triangle3Area = getTriangleArea(5, 8);
+
 
 }
 
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(-10, 15),
+	triangle3Area = getTriangleArea(5, 8);
+	
 console.log( getTriangleArea(10,6) );
-console.log( getTriangleArea(10,15) );
-console.log( getTriangleArea(-10,15) );
-console.log( getTriangleArea(5,8) );
+console.log( triangle1Area );
+console.log( triangle2Area );
+console.log( triangle3Area );
 
 
