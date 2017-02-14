d45a77d3179e38cba575c1fb2c011408717f2c58
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-14 20:14:35 +0100

    Events

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 ﻿var list = document.getElementById('list'),
     add = document.getElementById('addElem'),
     elementNumber = document.getElementsByClassName('item').length+1;
+
 add.addEventListener('click', function() {
-	list.innerHTML += '<li>item ' + elementNumber + '</li>'
-	elementNumber++
+	list.innerHTML += '<li>item ' + elementNumber + '</li>';
+	elementNumber++;
 });
